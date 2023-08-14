0b9b2b7e431cdacb0127acf76a13592f18ca35f2
Gzenxs <skykib@hotmail.com>
2023-08-13 21:34:36 -0600

    Modulo fs

---
+++ 06 setInterval()/app.js
@@ -0,0 +1,11 @@
+function mostrarTema(tema) {
+  console.log(`Estoy aprendiendo ${tema}`)
+}
+
+setInterval(mostrarTema, 1500, 'Node.js')
+
+function sumar(a, b) {
+  console.log(a + b);
+} 
+
+setInterval(sumar, 1700, 4, 5);
